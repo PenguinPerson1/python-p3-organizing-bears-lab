69d4388da954b6a3e7cc3ae723fd5183a929216e
Annania Plossl <nia.plossl@gmail.com>
2024-04-25 12:29:14 -0400

    completed the lab

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES("Mr. Chocolate",45,"M","brown","calm",TRUE);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES("Rowdy",7,"M","polar","rowdy",TRUE);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES("Tabitha",16,"F","black","calm",TRUE);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES("Sergeant Brown",30,"M","brown","rowdy",FALSE);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES("Melissa",35,"F","polar","rowdy",TRUE);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES("Grinch",90,"M","green","heartless",TRUE);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES("Wendy",13,"F","red","calm",FALSE);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES(NULL,2,"F","polar","calm",TRUE);
